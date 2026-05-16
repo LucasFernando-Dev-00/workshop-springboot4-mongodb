697d522ada04f6e6dc0e0bc78b6d827a170fd3fd
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-05-16 18:07:48 -0300

    Adicione a funcionalidade insert

--- src/main/java/com/projeto/workshopmongo/services/UserService.java
+++ src/main/java/com/projeto/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.projeto.workshopmongo.services;
 
 import com.projeto.workshopmongo.domain.User;
+import com.projeto.workshopmongo.dto.UserDTO;
 import com.projeto.workshopmongo.repositories.UserRepository;
 import com.projeto.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
         User user = repo.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
         return user;
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }

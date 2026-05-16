18a18e03907d8bf6a5ec19e8e284266a13f9153a
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-05-16 11:43:06 -0300

    Adicionei os recursos do User

---
+++ src/main/java/com/projeto/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.workshopmongo.repositories;
+
+import com.projeto.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}

b7c7d0288eabf01b79b8a95ea60cd499701e05bd
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-05-17 20:06:13 -0300

    Adicionei a requisição get para Post

--- src/main/java/com/projeto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/projeto/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.projeto.workshopmongo.resources;
 
+import com.projeto.workshopmongo.domain.Post;
 import com.projeto.workshopmongo.domain.User;
 import com.projeto.workshopmongo.dto.UserDTO;
 import com.projeto.workshopmongo.services.UserService;
@@ -54,6 +55,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity <List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
